9bb1ed003df8d9fe348852719e763c47ecedfb09
pgraffigna <pabloggraffigna@gmail.com>
2022-05-02 10:35:09 -0300

    primera version

---
+++ wireless.sh
@@ -0,0 +1,9 @@
+nmcli connection = lista las conexiones
+nmcli dev status = estado de las interfaces
+nmcli dev = muestra el estado de los dispositivos 
+
+nmcli radio wifi = estado de la placa inalambrica
+nmcli radio wifi on = activa la antena
+
+nmcli dev wifi list = escaneo de las redes
+nmcli dev wifi --ask connect SSID = conectar a una red
